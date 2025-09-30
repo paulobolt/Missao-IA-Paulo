cfade77bce631bc9e9fa2e0ba877dd5cd23f1b26
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-09-30 11:34:18 -0300

    34

---
+++ main.js
@@ -0,0 +1,67 @@
+const perguntas = [
+    {
+        enunciado: "Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+    {
+        enunciado: " Pergunta?",
+        alternativas: [
+            {
+                texto: " Texto/resposta ",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: " Texto/resposta ",
+                afirmacao: " Afirmação ",
+            }
+        ]
+    },
+];
