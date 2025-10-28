bbc2fad1dadce3ceff69cfcb10e7c3ea685a78c9
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-10-28 11:54:24 -0300

    28/10

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
-
-
-t
-
     {
-        enunciado: "Quando foi desenvolvido a primeira copa do mundo?
-",
+        enunciado: "Quando foi desenvolvido a primeira copa do mundo?",
         alternativas: [
             {
                 texto: " 13 de julho de 1930, no uruguai.",
@@ -70,3 +71,45 @@ t
         ]
     },
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = " ";
+
+function mostraPergunta(){
+
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    } 
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Se fosse possível traduzir sua forma de aprender em palavras, diríamos que...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = " ";
+}
+
+mostraPergunta();
